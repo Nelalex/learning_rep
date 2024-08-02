5cfd5da80e700ef43ed94ccb641fe67a29a4bc83
Nelalex <Nelalexxx@gmail.com>
2024-08-02 18:53:52 +0300

    Leetcode task 1556. Thousand Separator

--- src/Main.kt
+++ src/Main.kt
@@ -1,51 +1,22 @@
 fun main() {
-    val n = readln().toInt()
-    println(countLargestGroup(n).toString())
+    val n = readln().toLong()
+    println(thousandSeparator(n).toString())
 
 }
-//https://leetcode.com/problems/count-largest-group/description/
-//
-fun countLargestGroup(n: Int): Int {
-    val digitSizeMap = mutableMapOf<Int, MutableList<Int>>()
-    for (number in 1 .. n) {
-        val digitsSum = number.toString().map { it.digitToInt() }.sum()
-        digitSizeMap.computeIfAbsent(digitsSum) { mutableListOf()}.add(number)
-    }
 
-    var maxValue = 0
-    var maxValueCount = 0
-    for ((key, value) in digitSizeMap) {
-        when {
-            value.size > maxValue -> {
-                maxValue = value.size
-                maxValueCount = 1
-            }
-            value.size == maxValue -> {
-                maxValueCount ++
-            }
+    // https://leetcode.com/problems/thousand-separator/description/
+
+    fun thousandSeparator(n: Long): String {
+        val answer = n.toString().toCharArray().toMutableList()
+
+        val size = answer.size - 1
+        for (digit in size downTo 0) {
+            if ( (size - digit) % 3 == 0 && size - digit > 2)
+                answer.add(digit + 1, '.')
+
         }
+        return answer.joinToString(separator = "")
     }
 
-    return maxValueCount
-}
 
 
-/*
-fun countLargestGroup(n: Int): Int {
-    var maxValue: Int = 0
-    var maxValueCount: Int = 0
-    for (number in n downTo 1) {
-        val digitsSum = number.toString().map { it.digitToInt() }.sum()
-        val sum = (digitsSum.toString() + number.toString()).map { it.digitToInt() }.size
-        when {
-            sum > maxValue -> {
-                maxValueCount = 1
-                maxValue = sum
-            }
-            sum == maxValue -> {
-                maxValueCount++
-            }
-        }
-    }
-    return  maxValueCount
-}*/
