929c995c83354d196282a5573a39b522e0c87810
Nelalex <Nelalexxx@gmail.com>
2024-08-03 20:24:11 +0300

    Leetcode task 836. Rectangle Overlap

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,21 @@
-fun main() {
-    val n = readln().toLong()
-    println(thousandSeparator(n).toString())
 
+fun main() {
+    println(isRectangleOverlap(intArrayOf(0,0,2,2), intArrayOf(1,1,3,3)))
 }
 
-    // https://leetcode.com/problems/thousand-separator/description/
-
-    fun thousandSeparator(n: Long): String {
-        val answer = n.toString().toCharArray().toMutableList()
-
-        val size = answer.size - 1
-        for (digit in size downTo 0) {
-            if ( (size - digit) % 3 == 0 && size - digit > 2)
-                answer.add(digit + 1, '.')
-
-        }
-        return answer.joinToString(separator = "")
+// https://leetcode.com/problems/rectangle-overlap/description/
+fun isRectangleOverlap(rec1: IntArray, rec2: IntArray): Boolean {
+    return when{
+        rec1[0] >= rec2[2] -> false
+        rec1[1] >= rec2[3] -> false
+        rec2[0] >= rec1[2] -> false
+        rec2[1] >= rec1[3] -> false
+        else -> true
     }
+}
+
+
+
 
 
 
