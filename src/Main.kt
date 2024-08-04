c00e5add1b689f9e128adcb34da5af0172b262a9
Nelalex <Nelalexxx@gmail.com>
2024-08-04 09:59:52 +0300

    Leetcode task 2605. Form Smallest Number From Two Digit Arrays

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,20 @@
 
 fun main() {
-    println(isRectangleOverlap(intArrayOf(0,0,2,2), intArrayOf(1,1,3,3)))
+    println(minNumber(intArrayOf(0,0,2,2), intArrayOf(1,1,3,3)))
 }
 
-// https://leetcode.com/problems/rectangle-overlap/description/
-fun isRectangleOverlap(rec1: IntArray, rec2: IntArray): Boolean {
-    return when{
-        rec1[0] >= rec2[2] -> false
-        rec1[1] >= rec2[3] -> false
-        rec2[0] >= rec1[2] -> false
-        rec2[1] >= rec1[3] -> false
-        else -> true
-    }
+// https://leetcode.com/problems/form-smallest-number-from-two-digit-arrays/description/
+// I know that it's not effective not to store min values and always invoke methods, its just for practise
+fun minNumber(nums1: IntArray, nums2: IntArray): Int {
+    val minCommon = (nums1 intersect nums2.toSet())
+    if (minCommon.isNotEmpty())
+        return minCommon.min()
+    val minDigit = if (nums1.min() > nums2.min()) nums2.min() else nums1.min()
+    return if (minDigit == nums2.min())
+            (nums2.min() * 10 + nums1.min())
+    else
+            (nums1.min() * 10 + nums2.min())
+
 }
 
 
