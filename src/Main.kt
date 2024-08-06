67d78b473fe7c5bc2cc44d6e6e07e9e31a3dc65b
Nelalex <Nelalexxx@gmail.com>
2024-08-06 21:39:24 +0300

    Leetcode task 1363.  Largest Multiple of Three

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,44 @@
-
 // Leetcode №188 Pascal's Triangle
 fun main() {
-    generate(3)
-
+    println(largestMultipleOfThree(intArrayOf(3,3,5,5,8,8,8)))
 }
-fun generate(numRows: Int): List<List<Int>> {
-    val triangle = MutableList(numRows) { MutableList(it + 1) { 0 } }
-    for (i in 0 until numRows) {
-        for (j in 0..i) {
-            triangle[i][j] = if (j == 0 || j == i) 1 else triangle[i -1][j - 1] + triangle[i - 1][j]
+
+fun largestMultipleOfThree(digits: IntArray): String {
+    val result = digits.toMutableList()
+    if (result.size == 1 && result[0] % 3 != 0)
+        return ""
+    if (result.sum() == 0)
+        return "0"
+    if (result.sum() % 3 == 0)
+        return result.sortedDescending().joinToString("") else {
+        if (result.indexOf(result.sum() % 3) != -1) {
+            result.removeAt(result.indexOf(result.sum() % 3))
+            if (result.sum() != 0)
+                return result.sortedDescending().joinToString("")
+        } else {
+            for (numbers in result) {
+                if (numbers % 3 == result.sum() % 3 && numbers != 0) {
+                    result.remove(numbers)
+                    if (result.sum() != 0)
+                        return result.sortedDescending().joinToString("")
+                }
+
+            }
+            for (numbers in result.sorted()) {
+                if (result.sum() % 3 != 0) {
+                    if (numbers % 3 != 0)
+                    result.remove(numbers)
+                }
+
+            }
+
         }
     }
-    return triangle
+    if (result.size > 1 && result.sum() == 0)
+        return "0"
+    if (result.sum() == 0)
+        return ""
+    return result.sortedDescending().joinToString("")
 }
 
 
