ef08705f6a181f6aa0877a442372d6d66583208f
Nelalex <Nelalexxx@gmail.com>
2024-08-10 11:50:44 +0300

    Leetcode task 2894.  Divisible and Non-divisible Sums Difference

--- src/Main.kt
+++ src/Main.kt
@@ -1,44 +1,20 @@
 // Leetcode №188 Pascal's Triangle
 fun main() {
-    println(largestMultipleOfThree(intArrayOf(3,3,5,5,8,8,8)))
+    println(differenceOfSums(10, 3).toString())
 }
 
-fun largestMultipleOfThree(digits: IntArray): String {
-    val result = digits.toMutableList()
-    if (result.size == 1 && result[0] % 3 != 0)
-        return ""
-    if (result.sum() == 0)
-        return "0"
-    if (result.sum() % 3 == 0)
-        return result.sortedDescending().joinToString("") else {
-        if (result.indexOf(result.sum() % 3) != -1) {
-            result.removeAt(result.indexOf(result.sum() % 3))
-            if (result.sum() != 0)
-                return result.sortedDescending().joinToString("")
+
+fun differenceOfSums(n: Int, m: Int): Int {
+    var sum1 = 0
+    var sum2 = 0
+    for (i in 1 .. n) {
+        if (i % m == 0) {
+            sum2 += i
         } else {
-            for (numbers in result) {
-                if (numbers % 3 == result.sum() % 3 && numbers != 0) {
-                    result.remove(numbers)
-                    if (result.sum() != 0)
-                        return result.sortedDescending().joinToString("")
-                }
-
-            }
-            for (numbers in result.sorted()) {
-                if (result.sum() % 3 != 0) {
-                    if (numbers % 3 != 0)
-                    result.remove(numbers)
-                }
-
-            }
-
+            sum1 += i
         }
     }
-    if (result.size > 1 && result.sum() == 0)
-        return "0"
-    if (result.sum() == 0)
-        return ""
-    return result.sortedDescending().joinToString("")
+    return  sum1 - sum2
 }
 
 
