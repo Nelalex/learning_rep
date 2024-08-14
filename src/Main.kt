ebb89952f88b1448d3134d50009c749ec3d01e6c
Nelalex <Nelalexxx@gmail.com>
2024-08-14 19:15:33 +0300

    Leetcode task 121. Best time to Buy and Sell Stock

--- src/Main.kt
+++ src/Main.kt
@@ -1,37 +1,25 @@
 
 
-
-//Given two arrays of strings list1 and list2, find the common strings with the least index sum.
-//
-//A common string is a string that appeared in both list1 and list2.
-//
-//A common string with the least index sum is a common string such that if it appeared at list1[i] and list2[j] then i + j should be the minimum value among all the other common strings.
-//
-//Return all the common strings with the least index sum. Return the answer in any order.
-
-
 fun main() {
-    println(findRestaurant(arrayOf("Shogun","Tapioca Express","Burger King","KFC"), arrayOf("Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun")))
+    println(maxProfit(intArrayOf(1,2,3)))
 
 }
 
 
-fun findRestaurant(list1: Array<String>, list2: Array<String>): Array<String> {
-    var minValue = Int.MAX_VALUE
-    val minValueElements = emptyList<String>().toMutableList()
-    val commonElements = list1.toList().intersect(list2.toList())
-    for (element in commonElements) {
-        val indexSum = list1.indexOf(element) + list2.indexOf(element)
-        if  (indexSum == minValue) {
-            minValueElements.add(element)
-        }
-        if (indexSum < minValue) {
-            minValue = list1.indexOf(element) + list2.indexOf(element)
-            minValueElements.clear()
-            minValueElements.add(element)
-        }
+fun maxProfit(prices: IntArray): Int {
+    if (prices.isEmpty()) return 0
 
+    var minPrice = prices[0]
+    var maxProfit = 0
 
+    for (i in 1 until prices.size) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]
+        } else if (prices[i] - minPrice > maxProfit){
+            maxProfit = prices[i] - minPrice
+        }
     }
-    return minValueElements.toTypedArray()
+
+    return maxProfit
 }
+
