8a1079012b0e40058afd4012fb4ea30db9b602da
Nelalex <Nelalexxx@gmail.com>
2024-08-25 09:56:10 +0300

    Leetcode task 592. Fraction Addition and Subtraction

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,55 @@
+import kotlin.math.absoluteValue
 
 
 fun main() {
-    println(maxProfit(intArrayOf(1,2,3)))
+    println(fractionAddition("-5/2+10/3+7/9"))
+
+
 
 }
 
 
-fun maxProfit(prices: IntArray): Int {
-    if (prices.isEmpty()) return 0
+fun fractionAddition(expression: String): String {
+    val chislitel: MutableList<Int> = emptyList<Int>().toMutableList()
+    val znamenatel: MutableList<Int> = emptyList<Int>().toMutableList()
+    val newZnamenatel: MutableList<Int> = emptyList<Int>().toMutableList()
 
-    var minPrice = prices[0]
-    var maxProfit = 0
+    for (i in expression.indices) {
+        if (expression[i] == '/') {
 
-    for (i in 1 until prices.size) {
-        if (prices[i] < minPrice) {
-            minPrice = prices[i]
-        } else if (prices[i] - minPrice > maxProfit){
-            maxProfit = prices[i] - minPrice
+            if ( (i - 2) >= 0 && expression[i - 2] == '-')
+                chislitel.add(-expression[i - 1].digitToInt())
+            else
+                chislitel.add(expression[i - 1].digitToInt())
+            znamenatel.add(expression[i + 1].digitToInt())
         }
     }
+    newZnamenatel.addAll(znamenatel)
+    for (i in chislitel.indices) {
+        for (j in znamenatel.indices) {
+            if (i != j) {
+                chislitel[i] *= znamenatel[j]
+                newZnamenatel[i] *= znamenatel[j]
+            }
+        }
+    }
+    var otvetChislitel:Int = chislitel.sum()
+    var otvetZnametanel = newZnamenatel[0]
+    val nod = gcd(otvetChislitel,otvetZnametanel)
+    if (nod != 0) {
+        otvetChislitel /= nod
+        otvetZnametanel /= nod
+    }
+    var otvet = ""
+    if (otvetChislitel < 0 || otvetZnametanel < 0)
+        otvet += "-"
+    otvet  += "${otvetChislitel.absoluteValue}/${otvetZnametanel.absoluteValue}"
 
-    return maxProfit
+
+    return otvet
 }
 
+
+fun gcd(a: Int, b: Int): Int {
+    return if (b == 0) a else gcd(b, a % b)
+}
